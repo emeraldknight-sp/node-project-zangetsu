95c6b08f63377560f614dd7cab0d98a7c3a3c3d7
David <contato.davidalmeida@outlook.com>
2023-10-21 21:52:01 -0300

    🔥 Remove code or files

--- src/commands/desagree.js
+++
@@ -1,7 +0,0 @@
-export const desagreeCommand = (chatId, bot) => {
-  bot.sendMessage(
-    chatId,
-    `Ok, respeito sua posição. Infelizmente não poderemos dar continuidade ao seu cadastro.\n
-Pressione /start para iniciar novamente se mudar de ideia.`,
-  );
-};
--- src/commands/login.js
+++
@@ -1,3 +0,0 @@
-export const loginCommand = (chatId, bot) => {
-  bot.sendMessage(chatId, "OK. Você será logado!");
-};
